b835f3e6811de91fa010e39a4f6b74ad471b323b
Daniel Reeder <reederda@gmail.com>
2017-09-26 15:13:39 -0600

    initial commit

---
+++ main.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <set>
+
+using namespace std;
+
+int main(int argc, char *argv[]) {
+
+
+    if (argc < 2) {
+        cout << "Error: No input file provided" << endl;
+        return 1;
+    }
+
+    // open the input file
+    ifstream inputFile;
+    inputFile.open(argv[1], ifstream::in);
+
+    if ( inputFile.fail() ) {
+        cout << "File " << argv[1] << " not found, please try again." 
+	<< endl;
+        return -1;
+    }
+
+    set<string> outString;
+    string input;
+    // open an output file
+    ofstream outputFile;
+    outputFile.open("argv[1]_set.txt", ofstream::out);
+
+    // read the input file write it to the output file
+    while ( inputFile.peek() != EOF ) {
+        //getline(inputFile, outString);
+        outString.insert(input);
+	//outputFile << outString << endl;
+	outputFile.write(outString, 15);
+    }
+
+    inputFile.close();
+    outputFile.close();
+
+    cout << "A new file named " << argv[1] 
+	<< "_set.txt has been created."
+	<< endl;
+
+    return 0;
+}
