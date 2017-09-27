93f578cb182d96d9825fc20718b66e07ef42e27f
Daniel Reeder <reederda@gmail.com>
2017-09-26 23:07:01 -0600

    0.2

--- main.cpp
+++ main.cpp
@@ -2,18 +2,21 @@
 #include <fstream>
 #include <string>
 #include <set>
+#include <vector>
+#include <map>
+#include <list>
+#include <cstdlib>
 
 using namespace std;
 
 int main(int argc, char *argv[]) {
-
 
     if (argc < 2) {
         cout << "Error: No input file provided" << endl;
         return 1;
     }
 
-    // open the input file
+/************************ open the input file***********************/
     ifstream inputFile;
     inputFile.open(argv[1], ifstream::in);
 
@@ -22,27 +25,138 @@ int main(int argc, char *argv[]) {
 	<< endl;
         return -1;
     }
+/*******************************************************************/
+   
 
     set<string> outString;
+    vector<string> outVector;
+    map<string, string> wordmap;
+    string previous="";
     string input;
+    string state = "";
+    srand(time(NULL));
     // open an output file
     ofstream outputFile;
     outputFile.open("output.txt", ofstream::out);
 
-    // read the input file write it to the output file
-/*    while ( inputFile.peek() != EOF ) {
-        //getline(inputFile, outString);
-        outString.insert(input);
+    // read the input file into a string
+    while ( inputFile.peek() != EOF ) {
+	inputFile >> input;
+
+    //remove punctuation
+    for(int i=0; i < input.length(); i++)
+     {
+	if(ispunct(input[i]))
+	input.erase(i,1);
+     }
+
+    //change to all lowercase
+	for (int i = 0; i < input.size(); i++)
+	{
+           input[i] = tolower(input[i]);
+	}
+
+	outString.insert(input);
+    //cout << input << endl;
+
+        //outString.insert(input);
 	//outputFile << outString << endl;
 	//outputFile.write(outString, "file.txt");
+	outVector.push_back(input);
+	wordmap[previous] = input;
+	previous = input;
     }
+
+
+//displays contents of map
+/*	int i = 0;
+	std::map<string,string>::iterator iters = wordmap.begin();
+	while (i < wordmap.size())
+{
+	cout << (*iters).first << " " << (*iters).second << endl;
+	std::advance(iters,1);
+	i++;
+}
 */
-    inputFile.close();
-    outputFile.close();
+
+cout << endl << "Part 4: " << endl;
+for (int i = 0; i < 100; i++)
+{
+	cout << wordmap[state] << " ";
+	state = wordmap[state];
+}
+cout << endl;
+
+
+cout << endl << "Part 5: " << endl;
+map< string, vector<string> > wordmap2;
+string state2 = "";
+for (vector<string>::iterator it=outVector.begin(); it!=outVector.end(); it++)
+{
+	wordmap2[state2].push_back(*it);
+	state2 = *it;
+
+}
+
+
+vector<string>::iterator it=outVector.begin();
+	for (int i = 0; i < 6; i++)
+{
+	++it;
+	if (i = 5)
+	{
+	for (int j = 0; j < wordmap2[*it].size(); j++)
+	{
+	cout << wordmap2[*it][j] << " ";
+	}
+	}
+}
+
+cout << endl;
+
+cout << endl << "Part 6: " << endl;
+map< list<string>, vector<string> > wordmap3;
+list<string> state3;
+for (int i = 0; i < 2; i++)
+	state3.push_back("");
+
+for (vector<string>::iterator it2=outVector.begin(); it2!=outVector.end(); it2++)
+{
+	wordmap3[state3].push_back(*it2);
+	state3.push_back(*it2);
+	state3.pop_front();
+}
+
+state3.clear();
+
+for (int i = 0; i < 2; i++)
+state3.push_back("");
+
+for (int i = 0; i < 100; i++)
+{
+//	if (wordmap3.count(state3))
+//	cout << "ERROR\n";
+	int ind = rand() % wordmap3[state3].size();
+
+
+	cout << wordmap3[state3][ind] << " ";
+	state3.push_back(wordmap3[state3][ind]);
+	state3.pop_front();
+}
+cout << endl;
+
+
 
 //    cout << "A new file named " << argv[1] 
 //	<< "_set.txt has been created."
 //	<< endl;
 
+cout << endl;
+cout << "Words in this set: " << outString.size() << endl;
+cout << "Words in this vector: " << outVector.size() << endl;
+cout << "Keys in this map: " << wordmap.size() << endl;
+
+    inputFile.close();
+    outputFile.close();
     return 0;
 }
